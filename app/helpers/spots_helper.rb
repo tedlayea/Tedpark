f39d7f8f74e432039b44726ee8a536b77e5ca739
tedlayea <tedlayea@gMAIL.com>
2017-06-17 14:19:04 -0400

    google map included

--- app/helpers/spots_helper.rb
+++ app/helpers/spots_helper.rb
@@ -1,7 +1,9 @@
 module SpotsHelper
 def location_name (x,y)
+
   @res=Geokit::Geocoders::GoogleGeocoder.reverse_geocode "x,y"
   # @res= @res[1..50]
+  # http://maps.google.com/maps/api/staticmap?center=<%= lat%>,<%=long%>&zoom=19&size=600x600&maptype=roadmap&sensor=false
 end
 def location_coordinates
   @a = Geokit::Geocoders::GoogleGeocoder.geocode '140 Market St, San Francisco, CA'
